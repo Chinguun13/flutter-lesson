40d4ce7bd741338972deeae203365e087644eb0e
chegii <chinguunv@gmail.com>
2024-11-19 15:38:09 +0800

    feat: folder stractor

---
+++ lib/screens/profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Login Page"),
+    );
+  }
+}
---
+++ lib/screens/wishlist.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Wishlist extends StatelessWidget {
+  const Wishlist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Wish List Page"),
+    );
+  }
+}
+  
